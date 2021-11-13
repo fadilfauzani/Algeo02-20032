b3e0e8b86a0a85de2b524d47ebbb9aa0227c2eef
gibrandarmawan <88297926+gibrandarmawan@users.noreply.github.com>
2021-11-13 20:01:02 +0700

    Update app.py

--- src/flask/app.py
+++ src/flask/app.py
@@ -31,6 +31,37 @@ def upload_files():
 def upload(filename):
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
+# fungsi cari igen value 
+
+def igen(matrix):
+    x = np.zeros((len(matrix)))
+    for i in range(len(matrix)):
+        x[i] = 1
+    a = np.array(matrix)
+    toleransi = 0.001
+    maxsteps = 10
+
+    lambda_old = 1.0
+    cond = True
+    step= 1
+
+    while cond:
+        x = np.matmul(a,x)
+
+        lambdanew= max(abs(x))
+
+        x = x/lambdanew
+
+        step = step + 1
+        if step > maxsteps:
+            break
+        # Calculating error
+        error = abs(lambdanew - lambda_old)
+        lambda_old = lambdanew
+        condition = error > toleransi
+    
+    return lambdanew, x
+
 def compressImage(file):
     imge = Image.open(os.path.join(app.config['UPLOAD_PATH'], file))
     img = np.asarray(imge).astype(float)
@@ -39,14 +70,28 @@ def compressImage(file):
     b = img[:,:,2]
     N = min(r.shape)
     k = round(0.01 * N)
+    rows = len(r)
+    cols = len(r[0])
+
+    # kali transpose sm matrix awal
+    mulr = [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*r)] for X_row in r.transpose()]
+    mulg = [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*g)] for X_row in r.trasnpose()]
+    mulb = [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*b)] for X_row in r.transpose()]
+
+    igen(mulr)
+
+    
+
+
+    
 
     #!skrg masi pake numpy ntr diganti
-    ur,sr,vr = np.linalg.svd(r)
-    ug,sg,vg = np.linalg.svd(g)
-    ub,sb,vb = np.linalg.svd(b) 
-    rr = np.dot(ur[:,:k],np.dot(np.diag(sr[:k]), vr[:k,:])) 
-    rg = np.dot(ug[:,:k],np.dot(np.diag(sg[:k]), vg[:k,:]))
-    rb = np.dot(ub[:,:k],np.dot(np.diag(sb[:k]), vb[:k,:]))
+    # ur,sr,vr = np.linalg.svd(r)
+    # ug,sg,vg = np.linalg.svd(g)
+    # ub,sb,vb = np.linalg.svd(b) 
+    # rr = np.dot(ur[:,:k],np.dot(np.diag(sr[:k]), vr[:k,:])) 
+    # rg = np.dot(ug[:,:k],np.dot(np.diag(sg[:k]), vg[:k,:]))
+    # rb = np.dot(ub[:,:k],np.dot(np.diag(sb[:k]), vb[:k,:]))
         
     #merging
     rimg = np.zeros(img.shape)
